69c80cee1dbd8062c54d15d1bfce370af392ceb7
Lidia <lidia_tanase97@yahoo.com>
2024-06-25 20:53:39 +0300

    no tasks component

--- src/components/ContainerBox/ContainerBox.jsx
+++ src/components/ContainerBox/ContainerBox.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import CreateTaskForm from "../CreateTaskForm/CreateTaskForm";
 import TaskCard from "../TaskCard/TaskCard";
 import "./ContainerBox.css";
+import NoTasks from "../NoTasks/NoTasks";
 
 const ContainerBox = () => {
   const [taskList, setTaskList] = useState([]);
@@ -23,17 +24,22 @@ const ContainerBox = () => {
     <div className="container-box">
       <h1>Tasks</h1>
       <h4>Your tasks in your space.</h4>
-      <div className="tasks">
-        {taskList.map((task, index) => (
-          <TaskCard
-            name={task.taskName}
-            status={task.taskStatus}
-            desc={task.taskDesc}
-            date={task.taskDate}
-            key={index}
-          />
-        ))}
-      </div>
+      {/* {randare conditionala} */}
+      {taskList.length > 0 ? (
+        <div className="tasks">
+          {taskList.map((task, index) => (
+            <TaskCard
+              name={task.taskName}
+              status={task.taskStatus}
+              desc={task.taskDesc}
+              date={task.taskDate}
+              key={index}
+            />
+          ))}
+        </div>
+      ) : (
+        <NoTasks />
+      )}
       <CreateTaskForm addTask={onAddTask} />
     </div>
   );
---
+++ src/components/NoTasks/NoTasks.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./NoTasks.css";
+import Button from "../Button/Button";
+import noTasksImg from "../../assets/NoTasks.svg";
+
+const NoTasks = () => {
+  return (
+    <div className="noTasks">
+      <img src="/images/NoTasks.svg" alt="" />
+      {/* <img src={noTasksImg} alt="" /> */}
+      <h2>No Tasks Yet</h2>
+      <p>You have no task created in your workspace yet.</p>
+      <p>Get productive. Create a Task Now.</p>
+      <Button text="Create a Task" />
+    </div>
+  );
+};
+
+export default NoTasks;
